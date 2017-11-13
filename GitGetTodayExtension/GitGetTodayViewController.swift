72a4abd97840bbf5aeb1859cc5248555f90a1a44
fimuxd@gmail.com <fimuxd@gmail.com>
2017-11-13 23:49:11 +0900

    [추가] Month/Day Label 및 CollectionView UI 추가

--- GitGetTodayExtension/GitGetTodayViewController.swift
+++ GitGetTodayExtension/GitGetTodayViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TodayViewController.swift
+//  GitGetTodayViewController.swift
 //  GitGetTodayExtension
 //
 //  Created by Bo-Young PARK on 13/11/2017.
@@ -9,11 +9,49 @@
 import UIKit
 import NotificationCenter
 
-class TodayViewController: UIViewController, NCWidgetProviding {
-        
+class GitGetTodayViewController: UIViewController, NCWidgetProviding {
+    
+    /********************************************/
+    //MARK:-      Variation | IBOutlet          //
+    /********************************************/
+    
+    //월을 나타내는 Label: 총 12개
+    @IBOutlet weak var monthLabelOneByTwelve: UILabel!
+    @IBOutlet weak var monthLabelTwoByTwelve: UILabel!
+    @IBOutlet weak var monthLabelThreeByTwelve: UILabel!
+    @IBOutlet weak var monthLabelFourByTwelve: UILabel!
+    @IBOutlet weak var monthLabelFiveByTwelve: UILabel!
+    @IBOutlet weak var monthLabelSixByTwelve: UILabel!
+    @IBOutlet weak var monthLabelSevenByTwelve: UILabel!
+    @IBOutlet weak var monthLabelEightByTwelve: UILabel!
+    @IBOutlet weak var monthLabelNineByTwelve: UILabel!
+    @IBOutlet weak var monthLabelTenByTwelve: UILabel!
+    @IBOutlet weak var monthLabelElevenByTwelve: UILabel!
+    @IBOutlet weak var monthLabelTwelveByTwelve: UILabel!
+    
+    //요일을 나타내는 Label: 총 3개
+    @IBOutlet weak var mondayLabel: UILabel!
+    @IBOutlet weak var wednesdayLabel: UILabel!
+    @IBOutlet weak var fridayLabel: UILabel!
+    
+    //Contribution을 나타내는 CollectionView
+    @IBOutlet weak var contributionsCollectionView: UICollectionView!
+    
+    
+    /********************************************/
+    //MARK:-            LifeCycle               //
+    /********************************************/
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        contributionsCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "contributions")
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,6 +59,10 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    /********************************************/
+    //MARK:-       Methods | IBAction           //
+    /********************************************/
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
@@ -31,4 +73,23 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         completionHandler(NCUpdateResult.newData)
     }
     
+}
+
+//CollectionView 관련부분 구현할 부분
+extension GitGetTodayViewController:UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 366
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "contributions", for: indexPath)
+        
+        cell.backgroundColor = .black
+        
+        return cell
+    }
 }
