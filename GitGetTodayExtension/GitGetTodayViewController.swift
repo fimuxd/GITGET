6e1b7326f8330cf5e95c64f2248950a2e385f911
fimuxd@gmail.com <fimuxd@gmail.com>
2017-11-14 11:16:27 +0900

    [수정] TodayExtension 상 CollectionView 올리기 성공
    1. `.expended` 완료 (6개월 Contribution 표시)
    2. `.compact` 구현할 것 (일주일 Contribution 표시)

--- GitGetTodayExtension/GitGetTodayViewController.swift
+++ GitGetTodayExtension/GitGetTodayViewController.swift
@@ -1,57 +1,58 @@
 //
-//  GitGetTodayViewController.swift
+//  TodayViewController.swift
 //  GitGetTodayExtension
 //
-//  Created by Bo-Young PARK on 13/11/2017.
+//  Created by Bo-Young PARK on 14/11/2017.
 //  Copyright © 2017 Bo-Young PARK. All rights reserved.
 //
 
 import UIKit
 import NotificationCenter
 
-class GitGetTodayViewController: UIViewController, NCWidgetProviding {
+class TodayViewController: UIViewController, NCWidgetProviding {
     
     /********************************************/
     //MARK:-      Variation | IBOutlet          //
     /********************************************/
-    
-    //월을 나타내는 Label: 총 12개
-    @IBOutlet weak var monthLabelOneByTwelve: UILabel!
-    @IBOutlet weak var monthLabelTwoByTwelve: UILabel!
-    @IBOutlet weak var monthLabelThreeByTwelve: UILabel!
-    @IBOutlet weak var monthLabelFourByTwelve: UILabel!
-    @IBOutlet weak var monthLabelFiveByTwelve: UILabel!
-    @IBOutlet weak var monthLabelSixByTwelve: UILabel!
-    @IBOutlet weak var monthLabelSevenByTwelve: UILabel!
-    @IBOutlet weak var monthLabelEightByTwelve: UILabel!
-    @IBOutlet weak var monthLabelNineByTwelve: UILabel!
-    @IBOutlet weak var monthLabelTenByTwelve: UILabel!
-    @IBOutlet weak var monthLabelElevenByTwelve: UILabel!
-    @IBOutlet weak var monthLabelTwelveByTwelve: UILabel!
-    
-    //요일을 나타내는 Label: 총 3개
-    @IBOutlet weak var mondayLabel: UILabel!
-    @IBOutlet weak var wednesdayLabel: UILabel!
-    @IBOutlet weak var fridayLabel: UILabel!
-    
-    //Contribution을 나타내는 CollectionView
-    @IBOutlet weak var contributionsCollectionView: UICollectionView!
-    
+    var contributionCollectionView: UICollectionView!
+    let sectionInsets = UIEdgeInsets(top: 20, left: 13, bottom: 10, right: 3)
+    let itemsPerRow:CGFloat = 7
+    let leftSpace:CGFloat = 13
+    let rightSpace:CGFloat = 3
+    let numberOfWeeks:CGFloat = 26
+    let numberOfDaysPerWeek:CGFloat = 7
+    let minimumSpaceBetweenItems:CGFloat = 3
+    let paddingSpace:CGFloat = 10
     
     /********************************************/
     //MARK:-            LifeCycle               //
     /********************************************/
     override func viewDidLoad() {
         super.viewDidLoad()
+        extensionContext?.widgetLargestAvailableDisplayMode = .expanded
         
-
+        let layout:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = self.sectionInsets
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 2
+        layout.minimumInteritemSpacing = 2
+        
+        let availableWidth = (view.frame.width - self.leftSpace - self.rightSpace - ((self.numberOfWeeks-1) * self.minimumSpaceBetweenItems))
+        let widthPerItem = availableWidth/self.numberOfWeeks
+        layout.itemSize = CGSize(width: widthPerItem, height: widthPerItem)
+        
+        self.contributionCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        self.contributionCollectionView.dataSource = self
+        self.contributionCollectionView.delegate = self
+        self.contributionCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "contributions")
+        self.contributionCollectionView.backgroundColor = .clear
+        view.addSubview(self.contributionCollectionView)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        
-        contributionsCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "contributions")
-        
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let frame = view.frame
+        self.contributionCollectionView.frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: frame.size.width, height: frame.size.height)
     }
     
     override func didReceiveMemoryWarning() {
@@ -63,33 +64,48 @@ class GitGetTodayViewController: UIViewController, NCWidgetProviding {
     /********************************************/
     //MARK:-       Methods | IBAction           //
     /********************************************/
-    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        let availableWidth = (view.frame.width - self.leftSpace - self.rightSpace - ((self.numberOfWeeks-1) * self.minimumSpaceBetweenItems))
+        let widthPerItem = availableWidth/self.numberOfWeeks
+        let expendedContentHeight:CGFloat = self.sectionInsets.top + self.sectionInsets.bottom + (widthPerItem * self.numberOfDaysPerWeek) + (minimumSpaceBetweenItems * (self.numberOfDaysPerWeek - 1)) + self.paddingSpace
         
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
-        completionHandler(NCUpdateResult.newData)
+        if activeDisplayMode == .compact {
+            self.preferredContentSize = CGSize(width: 0, height: 110)
+        }else{
+            self.preferredContentSize = CGSize(width: 0, height: expendedContentHeight)
+        }
     }
     
 }
 
-//CollectionView 관련부분 구현할 부분
-extension GitGetTodayViewController:UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
+extension TodayViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 366
+        return 182
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "contributions", for: indexPath)
         
-        cell.backgroundColor = .black
-        
+        cell.layer.cornerRadius = 1
+        cell.backgroundColor = .blue
         return cell
     }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+////        let leftSpace:CGFloat = 13
+////        let rightSpace:CGFloat = 3
+////        let numberOfWeeks:CGFloat = 26
+////        let minimumSpaceBetweenItems:CGFloat = 3
+////
+////        let availableWidth = (view.frame.width - leftSpace - rightSpace - ((numberOfWeeks-1) * minimumSpaceBetweenItems))
+////        let widthPerItem = availableWidth/numberOfWeeks
+////
+////        return CGSize(width: widthPerItem, height: widthPerItem)
+//        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+//        let availableWidth = view.frame.width - paddingSpace
+//        let widthPerItem = availableWidth / itemsPerRow
+//
+//        return CGSize(width: widthPerItem, height: widthPerItem)
+//    }
 }
