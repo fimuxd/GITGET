67607a704894bd150e5b4a44827032bea9f75568
fimuxd@gmail.com <fimuxd@gmail.com>
2017-12-01 22:25:30 +0900

    [수정] 리팩토링 및 데이터구조 개선, 기기 사이즈별 위젯이 잘려 나오는 문제 해소

--- GitGetTodayExtension/GitGetTodayViewController.swift
+++ GitGetTodayExtension/GitGetTodayViewController.swift
@@ -13,11 +13,6 @@ import Alamofire
 import SwiftyJSON
 import SwiftSoup
 
-//TODO:- monthString 인간적으로 정리좀 하자 진심.
-//enum {
-//
-//}
-
 
 class TodayViewController: UIViewController, NCWidgetProviding {
     
@@ -140,10 +135,9 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         print("//TE_viewDidLoad")
-        extensionContext?.widgetLargestAvailableDisplayMode = .compact
         
+        extensionContext?.widgetLargestAvailableDisplayMode = .expanded
         self.contributionCollectionView.backgroundColor = .clear
         self.getMonthTextForLabel()
     }
@@ -233,7 +227,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         self.updateContributionDatasOf(gitHubID: realGitHubID)
     }
     
-    //위젯의 크기에 따라 표현하고 싶은 내용이 다를 때 사용. 현재 깃젯은 compact 사이즈만 제공함
+    //위젯의 크기에 따라 표현하고 싶은 내용이 다를 때 사용.
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
         if activeDisplayMode == .compact {
             /* .compact는 default 값으로, 높이 값이 정해져 있어 조절이 불가능 하다. (fixed 110px)
@@ -241,118 +235,39 @@ class TodayViewController: UIViewController, NCWidgetProviding {
              .expanded 를 사용하지 않고 .compact의 높이가 변화하는 예외 사항이 있다.
              iPhone의 Setting(설정) > General(일반) > Accessibility(손쉬운 사용) > Larger Text 의 하단에 있는 바를 통해 조절하는 경우다.
              고정된 높이(110px)에서 글자를 크게 조절할 수록 높이가 늘어나고, 작게 조절할 수록 높이는 줄어든다.
-             TODO:- 정확한 수치를 반영한 셀 조절은 추후 업데이트에 필요한 사항임
+             글씨 사이즈별로 위젯 높이는 8가지로 변화한다.
+             95, 100, 105, 110, 130, 145, 170, 200
             */
             self.preferredContentSize = maxSize
+        }else {
+            self.preferredContentSize = CGSize(width: 0, height: 110)
         }
     }
     
-    //TODO:- 이거 완전 미친 하드코딩임. enum을 쓰던 어떻게 해서 개선할 것
     func getMonthTextForLabel() {
         let date:Date = Date()
         let dateFormatter:DateFormatter = DateFormatter()
-        guard let timeZone:TimeZone = TimeZone(abbreviation: "UTC"),
-            let utcMonth = dateFormatter.calendar.dateComponents(in: timeZone, from: date).month else {return}
+        let currentMonth = dateFormatter.string(from: date)
         
-        switch utcMonth {
-        case 1:
-            self.currentMonthLabel.text = "Jan"
-            self.firstPreviousMonthLabel.text = "Dec"
-            self.secondPreviousMonthLabel.text = "Nov"
-            self.thirdPreviousMonthLabel.text = "Oct"
-            self.fourthPreviousMonthLabel.text = "Sep"
-            self.fifthPreviousMonthLabel.text = "Aug"
-            self.sixthPreviousMonthLabel.text = "Jul"
-        case 2:
-            self.currentMonthLabel.text = "Feb"
-            self.firstPreviousMonthLabel.text = "Jan"
-            self.secondPreviousMonthLabel.text = "Dec"
-            self.thirdPreviousMonthLabel.text = "Nov"
-            self.fourthPreviousMonthLabel.text = "Oct"
-            self.fifthPreviousMonthLabel.text = "Sep"
-            self.sixthPreviousMonthLabel.text = "Aug"
-        case 3:
-            self.currentMonthLabel.text = "Mar"
-            self.firstPreviousMonthLabel.text = "Feb"
-            self.secondPreviousMonthLabel.text = "Jan"
-            self.thirdPreviousMonthLabel.text = "Dec"
-            self.fourthPreviousMonthLabel.text = "Nov"
-            self.fifthPreviousMonthLabel.text = "Oct"
-            self.sixthPreviousMonthLabel.text = "Sep"
-        case 4:
-            self.currentMonthLabel.text = "Apr"
-            self.firstPreviousMonthLabel.text = "Mar"
-            self.secondPreviousMonthLabel.text = "Feb"
-            self.thirdPreviousMonthLabel.text = "Jan"
-            self.fourthPreviousMonthLabel.text = "Dec"
-            self.fifthPreviousMonthLabel.text = "Nov"
-            self.sixthPreviousMonthLabel.text = "Oct"
-        case 5:
-            self.currentMonthLabel.text = "May"
-            self.firstPreviousMonthLabel.text = "Apr"
-            self.secondPreviousMonthLabel.text = "Mar"
-            self.thirdPreviousMonthLabel.text = "Feb"
-            self.fourthPreviousMonthLabel.text = "Jan"
-            self.fifthPreviousMonthLabel.text = "Dec"
-            self.sixthPreviousMonthLabel.text = "Nov"
-        case 6:
-            self.currentMonthLabel.text = "Jun"
-            self.firstPreviousMonthLabel.text = "May"
-            self.secondPreviousMonthLabel.text = "Apr"
-            self.thirdPreviousMonthLabel.text = "Mar"
-            self.fourthPreviousMonthLabel.text = "Feb"
-            self.fifthPreviousMonthLabel.text = "Jan"
-            self.sixthPreviousMonthLabel.text = "Dec"
-        case 7:
-            self.currentMonthLabel.text = "Jul"
-            self.firstPreviousMonthLabel.text = "Jun"
-            self.secondPreviousMonthLabel.text = "May"
-            self.thirdPreviousMonthLabel.text = "Apr"
-            self.fourthPreviousMonthLabel.text = "Mar"
-            self.fifthPreviousMonthLabel.text = "Feb"
-            self.sixthPreviousMonthLabel.text = "Jan"
-        case 8:
-            self.currentMonthLabel.text = "Aug"
-            self.firstPreviousMonthLabel.text = "Jul"
-            self.secondPreviousMonthLabel.text = "Jun"
-            self.thirdPreviousMonthLabel.text = "May"
-            self.fourthPreviousMonthLabel.text = "Apr"
-            self.fifthPreviousMonthLabel.text = "Mar"
-            self.sixthPreviousMonthLabel.text = "Feb"
-        case 9:
-            self.currentMonthLabel.text = "Sep"
-            self.firstPreviousMonthLabel.text = "Aug"
-            self.secondPreviousMonthLabel.text = "Jul"
-            self.thirdPreviousMonthLabel.text = "Jun"
-            self.fourthPreviousMonthLabel.text = "May"
-            self.fifthPreviousMonthLabel.text = "Apr"
-            self.sixthPreviousMonthLabel.text = "Mar"
-        case 10:
-            self.currentMonthLabel.text = "Oct"
-            self.firstPreviousMonthLabel.text = "Sep"
-            self.secondPreviousMonthLabel.text = "Aug"
-            self.thirdPreviousMonthLabel.text = "Jul"
-            self.fourthPreviousMonthLabel.text = "Jun"
-            self.fifthPreviousMonthLabel.text = "May"
-            self.sixthPreviousMonthLabel.text = "Apr"
-        case 11:
-            self.currentMonthLabel.text = "Nov"
-            self.firstPreviousMonthLabel.text = "Oct"
-            self.secondPreviousMonthLabel.text = "Sep"
-            self.thirdPreviousMonthLabel.text = "Aug"
-            self.fourthPreviousMonthLabel.text = "Jul"
-            self.fifthPreviousMonthLabel.text = "Jun"
-            self.sixthPreviousMonthLabel.text = "Mar"
-        case 12:
-            self.currentMonthLabel.text = "Dec"
-            self.firstPreviousMonthLabel.text = "Nov"
-            self.secondPreviousMonthLabel.text = "Oct"
-            self.thirdPreviousMonthLabel.text = "Sep"
-            self.fourthPreviousMonthLabel.text = "Aug"
-            self.fifthPreviousMonthLabel.text = "Jul"
-            self.sixthPreviousMonthLabel.text = "Jun"
-        default: break
+        dateFormatter.dateFormat = "MMM"
+        dateFormatter.locale = Locale(identifier: "en_US") as Locale
+        
+        var dateArray:[Date] = [date]
+        for count in 1...6 {
+            let previousDate:Date = Date(timeInterval: -2592200 * fabs(Double(count)), since: date)
+            dateArray.append(previousDate)
         }
+        
+        var monthStringArray:[String] = dateArray.map { (date) -> String in
+            return dateFormatter.string(from: date)
+        }
+        self.currentMonthLabel.text = monthStringArray[0]
+        self.firstPreviousMonthLabel.text = monthStringArray[1]
+        self.secondPreviousMonthLabel.text = monthStringArray[2]
+        self.thirdPreviousMonthLabel.text = monthStringArray[3]
+        self.fourthPreviousMonthLabel.text = monthStringArray[4]
+        self.fifthPreviousMonthLabel.text = monthStringArray[5]
+        self.sixthPreviousMonthLabel.text = monthStringArray[6]
     }
     
     //각 월의 첫 날에 해당하는 셀의 x좌표와, 각 월별Label의 x좌표를 맞추는 함수
@@ -392,7 +307,6 @@ class TodayViewController: UIViewController, NCWidgetProviding {
                 }
                 self.sixthPreviousMonthLabel.isHidden = true
         case 304.0: //iPhone 4,5,SE
-            
             if xPositionArray[4] < 251 {
                 self.currentMonthLabelLeadingConstraint.constant = xPositionArray[4]
             }else{
@@ -447,7 +361,6 @@ class TodayViewController: UIViewController, NCWidgetProviding {
             self.dateArray = contributionsDateArray
         }
     }
-    
 }
 
 //MARK:- extension_CollectionView Delegate & DataSource
@@ -459,11 +372,11 @@ extension TodayViewController: UICollectionViewDelegateFlowLayout, UICollectionV
         guard let realDateArray = self.dateArray else {print("//날짜 값이 들어오지 않았습니다"); return 0}
         switch screenSize {
         case 398.0: //iPhone Plus
-            return realDateArray.count - 141
+            return realDateArray.count - 140
         case 359.0: //iPhone, X
-            return realDateArray.count - 162
+            return realDateArray.count - 161
         case 304.0: //iPhone SE, 4
-            return realDateArray.count - 197
+            return realDateArray.count - 196
         default:
             self.widgetStatusLabel.text = "Unable to Load"
             return 0
@@ -476,28 +389,28 @@ extension TodayViewController: UICollectionViewDelegateFlowLayout, UICollectionV
             let screenSize:CGFloat = self.view.frame.width
             switch screenSize {
             case 398.0: //iPhone Plus
-                cell.backgroundColor = UIColor(hex: realHexColorCodes[indexPath.row + 141])
+                cell.backgroundColor = UIColor(hex: realHexColorCodes[indexPath.row + 140])
                 
                 for index in 0...6 {
-                    if (indexPath.row + 141) == self.findIndexPathForFirstOf(previousMonthNumber: index) {
+                    if (indexPath.row + 140) == self.findIndexPathForFirstOf(previousMonthNumber: index) {
                         let xPosition:CGFloat = cell.frame.origin.x
                         self.xPositionForMonthLabels.append(xPosition)
                     }
                 }
             case 359.0: //iPhone, X
-                cell.backgroundColor = UIColor(hex: realHexColorCodes[indexPath.row + 162])
+                cell.backgroundColor = UIColor(hex: realHexColorCodes[indexPath.row + 161])
                 
                 for index in 0...5 {
-                    if (indexPath.row + 162) == self.findIndexPathForFirstOf(previousMonthNumber: index) {
+                    if (indexPath.row + 161) == self.findIndexPathForFirstOf(previousMonthNumber: index) {
                         let xPosition:CGFloat = cell.frame.origin.x
                         self.xPositionForMonthLabels.append(xPosition)
                     }
                 }
             case 304.0: //iPhone SE, 4
-                cell.backgroundColor = UIColor(hex: realHexColorCodes[indexPath.row + 197])
+                cell.backgroundColor = UIColor(hex: realHexColorCodes[indexPath.row + 196])
                 
                 for index in 0...4 {
-                    if (indexPath.row + 197) == self.findIndexPathForFirstOf(previousMonthNumber: index) {
+                    if (indexPath.row + 196) == self.findIndexPathForFirstOf(previousMonthNumber: index) {
                         let xPosition:CGFloat = cell.frame.origin.x
                         self.xPositionForMonthLabels.append(xPosition)
                     }
@@ -539,6 +452,108 @@ extension TodayViewController: UICollectionViewDelegateFlowLayout, UICollectionV
      self.utcWeekdayNumber = utcWeekDay
      
      print("//getUTCWeekdayFromLocalTime 함수 실행: 현재는 \(utcWeekDay)번째 요일입니다.")
+     }
+     */
+    
+    /* Delete: 정신나간 하드코딩이었다. :p
+     switch utcMonth {
+     case 1:
+     self.currentMonthLabel.text = "Jan"
+     self.firstPreviousMonthLabel.text = "Dec"
+     self.secondPreviousMonthLabel.text = "Nov"
+     self.thirdPreviousMonthLabel.text = "Oct"
+     self.fourthPreviousMonthLabel.text = "Sep"
+     self.fifthPreviousMonthLabel.text = "Aug"
+     self.sixthPreviousMonthLabel.text = "Jul"
+     case 2:
+     self.currentMonthLabel.text = "Feb"
+     self.firstPreviousMonthLabel.text = "Jan"
+     self.secondPreviousMonthLabel.text = "Dec"
+     self.thirdPreviousMonthLabel.text = "Nov"
+     self.fourthPreviousMonthLabel.text = "Oct"
+     self.fifthPreviousMonthLabel.text = "Sep"
+     self.sixthPreviousMonthLabel.text = "Aug"
+     case 3:
+     self.currentMonthLabel.text = "Mar"
+     self.firstPreviousMonthLabel.text = "Feb"
+     self.secondPreviousMonthLabel.text = "Jan"
+     self.thirdPreviousMonthLabel.text = "Dec"
+     self.fourthPreviousMonthLabel.text = "Nov"
+     self.fifthPreviousMonthLabel.text = "Oct"
+     self.sixthPreviousMonthLabel.text = "Sep"
+     case 4:
+     self.currentMonthLabel.text = "Apr"
+     self.firstPreviousMonthLabel.text = "Mar"
+     self.secondPreviousMonthLabel.text = "Feb"
+     self.thirdPreviousMonthLabel.text = "Jan"
+     self.fourthPreviousMonthLabel.text = "Dec"
+     self.fifthPreviousMonthLabel.text = "Nov"
+     self.sixthPreviousMonthLabel.text = "Oct"
+     case 5:
+     self.currentMonthLabel.text = "May"
+     self.firstPreviousMonthLabel.text = "Apr"
+     self.secondPreviousMonthLabel.text = "Mar"
+     self.thirdPreviousMonthLabel.text = "Feb"
+     self.fourthPreviousMonthLabel.text = "Jan"
+     self.fifthPreviousMonthLabel.text = "Dec"
+     self.sixthPreviousMonthLabel.text = "Nov"
+     case 6:
+     self.currentMonthLabel.text = "Jun"
+     self.firstPreviousMonthLabel.text = "May"
+     self.secondPreviousMonthLabel.text = "Apr"
+     self.thirdPreviousMonthLabel.text = "Mar"
+     self.fourthPreviousMonthLabel.text = "Feb"
+     self.fifthPreviousMonthLabel.text = "Jan"
+     self.sixthPreviousMonthLabel.text = "Dec"
+     case 7:
+     self.currentMonthLabel.text = "Jul"
+     self.firstPreviousMonthLabel.text = "Jun"
+     self.secondPreviousMonthLabel.text = "May"
+     self.thirdPreviousMonthLabel.text = "Apr"
+     self.fourthPreviousMonthLabel.text = "Mar"
+     self.fifthPreviousMonthLabel.text = "Feb"
+     self.sixthPreviousMonthLabel.text = "Jan"
+     case 8:
+     self.currentMonthLabel.text = "Aug"
+     self.firstPreviousMonthLabel.text = "Jul"
+     self.secondPreviousMonthLabel.text = "Jun"
+     self.thirdPreviousMonthLabel.text = "May"
+     self.fourthPreviousMonthLabel.text = "Apr"
+     self.fifthPreviousMonthLabel.text = "Mar"
+     self.sixthPreviousMonthLabel.text = "Feb"
+     case 9:
+     self.currentMonthLabel.text = "Sep"
+     self.firstPreviousMonthLabel.text = "Aug"
+     self.secondPreviousMonthLabel.text = "Jul"
+     self.thirdPreviousMonthLabel.text = "Jun"
+     self.fourthPreviousMonthLabel.text = "May"
+     self.fifthPreviousMonthLabel.text = "Apr"
+     self.sixthPreviousMonthLabel.text = "Mar"
+     case 10:
+     self.currentMonthLabel.text = "Oct"
+     self.firstPreviousMonthLabel.text = "Sep"
+     self.secondPreviousMonthLabel.text = "Aug"
+     self.thirdPreviousMonthLabel.text = "Jul"
+     self.fourthPreviousMonthLabel.text = "Jun"
+     self.fifthPreviousMonthLabel.text = "May"
+     self.sixthPreviousMonthLabel.text = "Apr"
+     case 11:
+     self.currentMonthLabel.text = "Nov"
+     self.firstPreviousMonthLabel.text = "Oct"
+     self.secondPreviousMonthLabel.text = "Sep"
+     self.thirdPreviousMonthLabel.text = "Aug"
+     self.fourthPreviousMonthLabel.text = "Jul"
+     self.fifthPreviousMonthLabel.text = "Jun"
+     self.sixthPreviousMonthLabel.text = "Mar"
+     case 12:
+     self.currentMonthLabel.text = "Dec"
+     self.firstPreviousMonthLabel.text = "Nov"
+     self.secondPreviousMonthLabel.text = "Oct"
+     self.thirdPreviousMonthLabel.text = "Sep"
+     self.fourthPreviousMonthLabel.text = "Aug"
+     self.fifthPreviousMonthLabel.text = "Jul"
+     self.sixthPreviousMonthLabel.text = "Jun"
+     default: break
      }
      */
     
