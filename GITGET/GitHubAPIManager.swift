2fa847a46bfaa8122653d9c00a279c0f39892cf9
fimuxd@gmail.com <fimuxd@gmail.com>
2017-12-05 02:20:37 +0900

    [수정] 데이터 관리

--- GITGET/GitHubAPIManager.swift
+++ GITGET/GitHubAPIManager.swift
@@ -33,19 +33,58 @@ class GitHubAPIManager {
         }
     }
     
+    func isFirstLogInForUpdate(completionHandler: @escaping(_ bool:Bool) -> Void) {
+        guard let currentUserUid:String = Auth.auth().currentUser?.uid else {return (completionHandler(true))}
+        Database.database().reference().child("UserInfo").child("\(currentUserUid)").child("gitHubID").observeSingleEvent(of: .value) { (snapshot) in
+            guard let realGitHubID:String = snapshot.value as? String else {return completionHandler(true)}
+           
+            completionHandler(false)
+        }
+    }
+    
     //GitHub API를 통해 데이터 불러오기
     //1. 현재 유저의 GitHubID
     func getCurrentGitHubID(completionHandler: @escaping (_ gitHubID:String) -> Void) {
         guard let currentUserUid:String = Auth.auth().currentUser?.uid else {print("//해당 UID에 해당하는 유저가 없습니다."); return}
         Database.database().reference().child("UserInfo").child("\(currentUserUid)").child("gitHubID").observeSingleEvent(of: .value) { (snapshot) in
-            guard let realGitHubID:String = snapshot.value as? String else {print("//해당 UID에 해당하는 유저가 없습니다."); return}
+            guard let realGitHubID:String = snapshot.value as? String,
+                let userDefault = UserDefaults(suiteName: "group.devfimuxd.TodayExtensionSharingDefaults") else {print("//해당 UID에 해당하는 유저가 없습니다."); return}
             
-            UserDefaults.standard.setValue(realGitHubID, forKey: "GitHubID")
+            userDefault.setValue(realGitHubID, forKey: "GitHubID")
             completionHandler(realGitHubID)
         }
     }
     
-    //2. 기본 userProfile 데이터
+    //2.1 새 userData 저장
+    func getGitHubIDForNewbie(with AccessToken:String, by currentUserUid:String, completionHandler: @escaping (_ gitHubID:String) -> Void) {
+        guard let getAuthenticatedUserUrl:URL = URL(string:"https://api.github.com/user") else {return}
+        let headers:HTTPHeaders = ["authorization":"Bearer \(AccessToken)"]
+        
+        Alamofire.request(getAuthenticatedUserUrl, method: .get, headers: headers).responseJSON { [unowned self] (response) in
+            guard let data:Data = response.data else {return}
+            let userInfoJson:JSON = JSON(data:data)
+            let gitHubID = userInfoJson["login"].stringValue
+            
+            let userInfo = ["gitHubID":gitHubID]
+            
+            //가져온 정보를 Firebase에 저장
+            Database.database().reference().child("UserInfo").child("\(currentUserUid)").setValue(userInfo)
+            
+            //GitHubID를 받아서 해당 유저의 Contributions를 수집하도록 함
+            //TODO:- 추후에 로그인 계정이 User인지 Corp(Team) 인지 구별하여 별도 처리하도록 함.
+            //       이유는, Contributions 가져오는 주소가 다른 것으로 알고 있음. (
+            //       개인: https://github.com/users{/username}/contributions
+            //       단체: 개인사이트 같은 별도 뷰는 없음. 비슷한 형태는, https://github.com{/organization_name}{/repository_name}/graphs/contributors
+            
+            guard let userDefaults = UserDefaults(suiteName: "group.devfimuxd.TodayExtensionSharingDefaults") else {return}
+            userDefaults.setValue(gitHubID, forKey: "GitHubID")
+            userDefaults.synchronize()
+            
+            completionHandler(gitHubID)
+        }
+    }
+    
+    //2.2 기본 userProfile 데이터
     func getCurrentUserDatas(completionHandler: @escaping (_ userDatas:[String:String]) -> Void) {
         self.getCurrentGitHubID { (realID) in
             guard let getCurrentUserDataUrl:URL = URL(string: "https://api.github.com/users/\(realID)"),
@@ -64,7 +103,8 @@ class GitHubAPIManager {
                 let location:String = json["location"].stringValue
                 let profileImageUrl:String = json["avatar_url"].stringValue
                 
-                let userDatas:[String:String] = ["name":name,
+                let userDatas:[String:String] = ["githubID":realID,
+                                                 "name":name,
                                                "email":email,
                                                "bio":bio,
                                                "url":url,
@@ -77,28 +117,30 @@ class GitHubAPIManager {
     }
     
     //3. Today Contributions Count
-    func getTodayContributionsCount(gitHubID:String, completionHandler: @escaping(_ todayContributionsCount: String) -> Void) {
-        guard let getContributionsUrl:URL = URL(string: "https://github.com/users/\(gitHubID)/contributions") else {return}
-        
-        Alamofire.request(getContributionsUrl, method: .get).responseString {(response) in
-            switch response.result {
-            case .success(let value):
-                do{
-                    let htmlValue = value
-                    guard let elements:Elements = try? SwiftSoup.parse(htmlValue).select("rect") else {return}
-                    var tempArray:[String] = []
-                    
-                    for element:Element in elements.array() {
-                        guard let dataCount:String = try? element.attr("data-count") else {return}
-                        tempArray.append(dataCount)
+    func getTodayContributionsCount(completionHandler: @escaping(_ todayContributionsCount: String) -> Void) {
+        self.getCurrentGitHubID { (realID) in
+            guard let getContributionsUrl:URL = URL(string: "https://github.com/users/\(realID)/contributions") else {return}
+            
+            Alamofire.request(getContributionsUrl, method: .get).responseString {(response) in
+                switch response.result {
+                case .success(let value):
+                    do{
+                        let htmlValue = value
+                        guard let elements:Elements = try? SwiftSoup.parse(htmlValue).select("rect") else {return}
+                        var tempArray:[String] = []
+                        
+                        for element:Element in elements.array() {
+                            guard let dataCount:String = try? element.attr("data-count") else {return}
+                            tempArray.append(dataCount)
+                        }
+                        
+                        guard let todayContributionsCount:String = tempArray.last else {return}
+                        
+                        completionHandler(todayContributionsCount)
                     }
-                    
-                    guard let todayContributionsCount:String = tempArray.last else {return}
-                    
-                    completionHandler(todayContributionsCount)
+                case .failure(let error):
+                    print("///Alamofire.request - error: ", error)
                 }
-            case .failure(let error):
-                print("///Alamofire.request - error: ", error)
             }
         }
     }
