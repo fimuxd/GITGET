3825df2c6a92eca28b16ff94a7931009ab51563a
fimuxd@gmail.com <fimuxd@gmail.com>
2017-12-06 14:37:34 +0900

    [추가] Setting Tab에서 프로필 눌렀을 때 Detail TableView로 이동

--- GITGET/GitHubAccountSettingTableViewController.swift
+++ GITGET/GitHubAccountSettingTableViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class GitHubAccountSettingTableViewController: UITableViewController {
     
-    let cellTitleData:[String] = []
+    let sectionHeadTitleData:[String] = ["Profile", "Starred Repositories"]
+    var repositoriesDatas:[[String:Any]]? {
+        didSet{
+            guard let realRepositoriesDatas = repositoriesDatas else {return}
+            repositoriesDatas = realRepositoriesDatas
+            
+            self.tableView.reloadData()
+        }
+    }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        GitHubAPIManager.sharedInstance.getStaredRepositoriesDataArray { (repositoryData) in
+            self.repositoriesDatas = repositoryData
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,69 +38,112 @@ class GitHubAccountSettingTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         
     }
-
+    
     
     
     // MARK: - Table view data source
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sectionHeadTitleData.count
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 6
+        switch section {
+        case 0:
+            return 6
+        case 1:
+            guard let realRepositoriesDatas = repositoriesDatas else {return 0}
+            return realRepositoriesDatas.count
+        default:
+            return 0
+        }
     }
-
-    /*
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionHeadTitleData[section]
+    }
+    
+//    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+//        return "Modified contents will be synced with GitHub except Email."
+//    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let modifiableCell:CustomTableViewCell = tableView.dequeueReusableCell(withIdentifier: "modifiableCell") as! CustomTableViewCell
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        switch indexPath.section {
+        case 0:
+            let cellTitles:[String] = ["Name", "Email", "Bio", "URL", "Company", "Location"]
+            modifiableCell.modifiableTitleLabel.text = cellTitles[indexPath.row]
+            
+            GitHubAPIManager.sharedInstance.getCurrentUserDatas(completionHandler: { (userDatas) in
+                let name:String? = userDatas["name"]
+                let email:String? = userDatas["email"]
+                let bio:String? = userDatas["bio"]
+                let url:String? = userDatas["url"]
+                let company:String? = userDatas["company"]
+                let location:String? = userDatas["location"]
+                var tempUserDataArray:[String?] = [name, email, bio, url, company, location]
+                
+                DispatchQueue.main.async {
+                    modifiableCell.modifiableTextField.text = tempUserDataArray[indexPath.row]
+                }
+                
+//                if indexPath.row == 1 { //이메일 셀은 내용 수정 불가
+                    modifiableCell.modifiableTextField.isUserInteractionEnabled = false
+//                }
+            })
+            
+            return modifiableCell
+        case 1:
+            guard let realRepositoriesDatas = repositoriesDatas,
+                let currentGitHubID:String? = UserDefaults(suiteName: "group.devfimuxd.TodayExtensionSharingDefaults")?.value(forKey: "GitHubID") as? String else {return cell}
+            
+            let repositoryName = realRepositoriesDatas[indexPath.row]["name"] as? String ?? ""
+            let repositoryDescription = realRepositoriesDatas[indexPath.row]["description"] as? String ?? ""
+            let repositoryFullName = realRepositoriesDatas[indexPath.row]["fullName"] as? String ?? ""
+            let repositoryowner = realRepositoriesDatas[indexPath.row]["owner"] as? String ?? ""
+            
+            if currentGitHubID != repositoryowner {
+                cell.textLabel?.text = repositoryFullName
+            }else{
+                cell.textLabel?.text = realRepositoriesDatas[indexPath.row]["name"] as? String ?? ""
+            }
+            
+            cell.detailTextLabel?.text = realRepositoriesDatas[indexPath.row]["description"] as? String ?? ""
+            
+            return cell
+        default:
+            return cell
+        }
+        
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+//    @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
+//        //키보드 내려가는 것 실행 (delegate)
+//        self.view.endEditing(true)
+//
+//        //TODO:- 텍스트 필드상의 내용을 GitHub API 에 POST
+//
+//    }
+    
+    
 }
+
+extension GitHubAccountSettingTableViewController:UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        <#code#>
+//    }
+}
+
