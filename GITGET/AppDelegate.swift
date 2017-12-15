468417b946455ade0d3ae6c71322f3770656569a
fimuxd@gmail.com <fimuxd@gmail.com>
2017-12-15 13:14:48 +0900

    [추가] 매일 저녁 10시에 오늘의 Contributions 알려주는 Notification 추가

--- GITGET/AppDelegate.swift
+++ GITGET/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 import Firebase
 import FirebaseAuth
 import Alamofire
@@ -22,7 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         print("//applicationDidFinishLaunchingWithOptions")
-    
+        
+        //MARK:- 로그인설정: 접속한 사용자가 신규가입자인지 기존가입자인지에 따라 rootViewController를 다르게 설정
         let currentUserUid = Auth.auth().currentUser?.uid
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
@@ -38,6 +40,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.window?.rootViewController = navigationController
             self.window?.makeKeyAndVisible()
         }
+        
+        self.setNotification(application: application)
         
         return true
     }
@@ -63,5 +67,63 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
+}
+
+//MARK:- Notification 설정
+extension AppDelegate:UNUserNotificationCenterDelegate {
+    
+    func setNotification(application:UIApplication) {
+        if #available(iOS 10.0, *) {
+            let notificationCenter = UNUserNotificationCenter.current()
+            notificationCenter.delegate = self
+            
+            notificationCenter.requestAuthorization(options: [.alert,.sound,.badge], completionHandler: { (granted, error) in
+                if granted { //알림 On
+                    
+                    GitHubAPIManager.sharedInstance.getTodayContributionsCount(completionHandler: { (todayContributions) in
+                        
+                        let notificationContent = UNMutableNotificationContent()
+                        notificationContent.sound = UNNotificationSound.default()
+                        notificationContent.title = "Check Your Today Contributions".localized
+                        
+                        switch Int(todayContributions)! {
+                        case 0:
+                            notificationContent.body = "😔 Oh no. You don't have any commits today.".localized
+                        case 1...5:
+                            notificationContent.body = String(format:NSLocalizedString("👍 Good. %@ contributions today!", comment: ""),todayContributions)
+                        case 6...19:
+                            notificationContent.body = String(format:NSLocalizedString("👏 Well done. %@ contributions today!", comment: ""),todayContributions)
+                        default:
+                            notificationContent.body = String(format:NSLocalizedString("🔥 Burned out! %@ contributions today!", comment: ""),todayContributions)
+                        }
+                        
+                        var notificationDateComponents = DateComponents()
+                        notificationDateComponents.hour = 13
+                        notificationDateComponents.minute = 14
+                        
+                        let notificationTrigger = UNCalendarNotificationTrigger(dateMatching: notificationDateComponents, repeats: true)
+                        let nightRequest:UNNotificationRequest = UNNotificationRequest(identifier: "GitGet", content: notificationContent, trigger: notificationTrigger)
+                        
+                        UNUserNotificationCenter.current().add(nightRequest, withCompletionHandler: { (_) in
+                            UNUserNotificationCenter.current().getPendingNotificationRequests { (notificationRequests) in
+                                print("///// notificationRequests.count- 8923: \n", notificationRequests.count)
+                                print("///// notificationRequests detail- 8923: \n", notificationRequests)
+                            }
+                        })
+                    })
+                    
+                    UserDefaults.standard.set(true, forKey: "settingAlarmOnOff")
+                    
+                }else{ //알림 Off
+                    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["GitGet"])
+                    UserDefaults.standard.set(false, forKey: "settingAlarmOnOff")
+                }
+            })
+            
+            application.registerForRemoteNotifications()
+        }else{ //iOS10 미만일 경우 미지원
+            //TODO:- 추후 10 이하 버전시 로컬 노티 하는 방법 알아볼 것
+        }
+    }
 }
 
