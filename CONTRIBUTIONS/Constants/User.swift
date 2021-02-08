b10d95ce2cb231cc6b754fd3bd421162c582cfda
fimuxd <me@boyoung.dev>
2021-02-08 11:36:35 +0900

    add entities

--- CONTRIBUTIONS/Constants/User.swift
+++ CONTRIBUTIONS/Constants/User.swift
@@ -10,9 +10,29 @@ import Foundation
 struct User: Codable {
     let name: String
     let profileImageURL: String
+    let bio: String?
+    let location: String?
+    let company: String?
+    let followers: Int
+    let following: Int
+    let createdAt: Date?
     
     enum CodingKeys: String, CodingKey {
-        case name
+        case name, bio, location, company, followers, following
         case profileImageURL = "avatar_url"
+        case createdAt = "created_at"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.name = try values.decode(String.self, forKey: .name)
+        self.profileImageURL = try values.decode(String.self, forKey: .profileImageURL)
+        self.bio = try? values.decode(String.self, forKey: .bio)
+        self.location = try? values.decode(String.self, forKey: .location)
+        self.company = try? values.decode(String.self, forKey: .company)
+        self.followers = try values.decode(Int.self, forKey: .followers)
+        self.following = try values.decode(Int.self, forKey: .following)
+        self.createdAt = Date.parse(values, key: .createdAt)
     }
 }
